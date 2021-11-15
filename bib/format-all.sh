6341623a846459d43a90055dc6a2085b21b53883
timtro <timtro@gmail.com>
2021-11-15 13:32:37 -0500

    Automated linting/formatting/styling.

---
+++ bib/format-all.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# vim: foldmethod=marker
+
+# Style arguments {{{
+style=('--curly '
+         '--numeric'
+         '--space=2'
+         '--align=0'
+         '--duplicates=key,doi,citation'
+         '--no-escape'
+         '--remove-empty-fields'
+        )
+# }}}
+# Order for field sorting {{{
+field_order=('author,'
+             'title,'
+             'shorttitle,'
+             'booktitle,'
+             'journal,'
+             'edition,'
+             'series,'
+             'volume,'
+             'number,'
+             'school,'
+             'institution,'
+             'doi,'
+             'isbn,'
+             'issn,'
+             'lccn,'
+             'location,'
+             'publisher,'
+             'address,'
+             'month,'
+             'day,'
+             'pages,'
+             'on,'
+             'holder,'
+             'articleno,'
+             'copyright,'
+             'category,'
+             'issue_date,'
+             'numpages,'
+             'editor,'
+             'keywords,'
+             'version,'
+             'eprint,'
+             'eprinttype,'
+             'eid,'
+             'archiveprefix,'
+             'primaryclass,'
+             'acmid,'
+             'language,'
+             'note,'
+             'metadata,'
+             'url,'
+             'urldate,'
+             'year'
+            )
+# }}}
+
+field_order="${field_order[*]}" # Join array with spaces.
+field_order="${field_order// /}" # Remove spaces.
+sorting="--sort=key --sort-fields=$field_order"
+args="${style[*]} $sorting"
+echo "bibtex-tidy $args  *.bib"
+bibtex-tidy $args *.bib
